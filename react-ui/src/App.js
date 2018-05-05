26ea06b15a8c0de5b0d0dca66f1d3a0d4cc29c4e
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-05 18:51:51 +0000

    Flesh out structure of app and put header into separate NavBar file

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logoNudge.png';
 import './App.css';
+// import Main from './Main.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      email: "",
+      lastMouseX: 0,
+      lastMouseY: 0,
+      contactFirstName: "",
+      contactEmail: ""
+    }
+    // bind 'this' here
+  }
+
+  // function to change firstName, email, contactFirstName, contactEmail at login
+  // function to change firstName, email, contactFirstName, contactEmail at registration
+  // function to change mouse coordinates
+  // function to check if user logged in (for conditionals)
+
+  // when cookie session is set, use it's user_id to fetch info to be stored in state
+
+  // componentDidMount() {
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <NavBar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
