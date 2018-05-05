24190b64c4ab8fc4f8b4d67d446e59b0a021e6c6
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-05 23:17:42 +0000

    Fix registration form

--- react-ui/src/Registration.jsx
+++ react-ui/src/Registration.jsx
@@ -24,26 +24,41 @@ export default class Login extends Component {
 
     return (
       <form className="registration">
-        <h1>You</h1>
+        <h1>Register</h1>
+        <h2>You</h2>
+        <div className="form-field">
         <label>First Name</label>
-        <input className="name" value={ this.state.firstName } placeholder="Jane" />
-        <label>Last Name</label>
-        <input className="name" value={ this.state.lastName } placeholder="Doe" />
-        <label>Email</label>
-        <input className="email" value={ this.state.email } placeholder="email@example.com" />
-        <label>Password</label>
-        <input className="password" value={ this.state.password } placeholder="Password" />
-        <label>Password Confirmation</label>
-        <input className="password-confirmation" value={ this.state.passwordConfirmation } placeholder="Password" />
-
-        <h1>Your emergency contact</h1>
-        <label>First Name</label>
-        <input className="name" value={ this.state.firstName } placeholder="Jane" />
-        <label>Last Name</label>
-        <input className="name" value={ this.state.lastName } placeholder="Doe" />
-        <label>Email</label>
-        <input className="email" value={ this.state.email } placeholder="email@example.com" />
-
+        <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Last Name</label>
+          <input className="name" value={ this.state.lastName } placeholder="Doe" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Email</label>
+          <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Password</label>
+          <input className="password" value={ this.state.password } placeholder="Password" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Password Confirmation</label>
+          <input className="password-confirmation" value={ this.state.passwordConfirmation } placeholder="Password" /><br/>
+        </div>
+        <h2>Your emergency contact</h2>
+        <div className="form-field">
+          <label>First Name</label>
+          <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Last Name</label>
+          <input className="name" value={ this.state.lastName } placeholder="Doe" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Email</label>
+          <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
+        </div>
         <button type="submit">Login</button>
       </form>
     );
