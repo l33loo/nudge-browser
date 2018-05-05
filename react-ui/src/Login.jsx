d5ba2acc12afae9b8f8641230651a7547883fbb2
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-05 20:07:53 +0000

    Add login form

---
+++ react-ui/src/Login.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = { email: "", password: "" };
+
+    // bind this here
+  }
+
+  render() {
+    // const onSubmit = e => {
+    //   e.preventDefault();
+    // };
+
+    return (
+      <fieldset>
+      <form className="login">
+        <label>Email</label>
+        <input className="login-email" value={ this.state.email } placeholder="email@example.com" />
+        <label>Password</label>
+        <input className="login-password" value={ this.state.password } placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+      </fieldset>
+    );
+  }
+
+}
