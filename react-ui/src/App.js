c2c710a850ff6c2a8544ad2a64bae1984d0b7478
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-05 20:09:40 +0000

    Remove template text

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -32,9 +32,6 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Login />
       </div>
     );
