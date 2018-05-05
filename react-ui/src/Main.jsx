f16229e2edb944cbafa1e30aea6b14dd9e3dac26
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-05 18:52:40 +0000

    Comment everything out

---
+++ react-ui/src/Main.jsx
@@ -0,0 +1,8 @@
+// import React, { Component } from 'react';
+
+// export default class Main extends Component {
+//   constructor({ props }) {
+//     super({ props });
+//     this.state = { userId: "", FirstName: "" };
+//   }
+// }
