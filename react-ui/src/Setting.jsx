093a3847132b164f2787864188fe2a9a0167a2d4
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-06 21:33:58 +0000

    Add form

--- react-ui/src/Setting.jsx
+++ react-ui/src/Setting.jsx
@@ -8,9 +8,34 @@ export default class Setting extends Component {
         lastName: "",
         email: "",
         contactName: "",
-        contactName: "",
         contactEmail: ""
       };
 
     // bind 'this' here
   }
+
+  // componentDidMount() {
+  // }
+
+  render() {
+    return (
+      <form className="settings">
+        <h1>Settings</h1>
+        <h2>Your emergency contact</h2>
+        <div className="form-field">
+          <label>Contact's Name</label>
+          <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
+        </div>
+        <div className="form-field">
+          <label>Contact's Email</label>
+          <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
+        </div>
+        <div>
+          <input type="checkbox" value={ this.state.notificationsEnabled } />
+          <label for="notifications"> Check if you love this website!</label>
+        </div>
+        <button type="submit">Edit</button>
+      </form>
+    );
+  }
+}
