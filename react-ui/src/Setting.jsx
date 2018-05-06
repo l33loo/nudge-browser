cc18c89ee5b3aa3a3dcf8d4c27891fc6d56221ec
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-06 19:47:57 +0000

    Import React and flesh out constructor

---
+++ react-ui/src/Setting.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Setting extends Component {
+  constructor() {
+    super();
+    this.state = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        contactName: "",
+        contactName: "",
+        contactEmail: ""
+      };
+
+    // bind 'this' here
+  }
