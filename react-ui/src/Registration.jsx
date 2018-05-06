fb2210f614297780383ee35231267d37a56fede0
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-06 23:19:14 +0000

    Add navigation between different components

--- react-ui/src/Registration.jsx
+++ react-ui/src/Registration.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
         firstName: "",
         lastName: "",
@@ -13,7 +13,11 @@ export default class Login extends Component {
         contactEmail: ""
       };
 
-    // bind 'this' here
+    this.linkLogin = this.linkLogin.bind(this);
+  }
+
+  linkLogin() {
+    this.props.renderPage("Login");
   }
 
   render() {
@@ -22,40 +26,43 @@ export default class Login extends Component {
     // };
 
     return (
-      <form className="registration">
-        <h1>Register</h1>
-        <h2>You</h2>
-        <div className="form-field">
-        <label>First Name</label>
-        <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
-        </div>
-        <div className="form-field">
-          <label>Last Name</label>
-          <input className="name" value={ this.state.lastName } placeholder="Doe" /><br/>
-        </div>
-        <div className="form-field">
-          <label>Email</label>
-          <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
-        </div>
-        <div className="form-field">
-          <label>Password</label>
-          <input className="password" value={ this.state.password } placeholder="Password" /><br/>
-        </div>
-        <div className="form-field">
-          <label>Password Confirmation</label>
-          <input className="password-confirmation" value={ this.state.passwordConfirmation } placeholder="Password" /><br/>
-        </div>
-        <h2>Your emergency contact</h2>
-        <div className="form-field">
-          <label>Contact's Name</label>
+      <div>
+        <form className="registration">
+          <h1>Register</h1>
+          <h2>You</h2>
+          <div className="form-field">
+          <label>First Name</label>
           <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
-        </div>
-        <div className="form-field">
-          <label>Contact's Email</label>
-          <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
-        </div>
-        <button type="submit">Login</button>
-      </form>
+          </div>
+          <div className="form-field">
+            <label>Last Name</label>
+            <input className="name" value={ this.state.lastName } placeholder="Doe" /><br/>
+          </div>
+          <div className="form-field">
+            <label>Email</label>
+            <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
+          </div>
+          <div className="form-field">
+            <label>Password</label>
+            <input className="password" value={ this.state.password } placeholder="Password" /><br/>
+          </div>
+          <div className="form-field">
+            <label>Password Confirmation</label>
+            <input className="password-confirmation" value={ this.state.passwordConfirmation } placeholder="Password" /><br/>
+          </div>
+          <h2>Your emergency contact</h2>
+          <div className="form-field">
+            <label>Contact's Name</label>
+            <input className="name" value={ this.state.firstName } placeholder="Jane" /><br/>
+          </div>
+          <div className="form-field">
+            <label>Contact's Email</label>
+            <input className="email" value={ this.state.email } placeholder="email@example.com" /><br/>
+          </div>
+          <button type="submit" style={{cursor:'pointer'}}>Login</button>
+        </form>
+       <div>Already a user yet? <span className="formLink" onClick={ this.linkLogin } style={{cursor:'pointer'}}>Login here.</span></div>
+      </div>
     );
   }
 }
