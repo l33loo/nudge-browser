9808c7260a7c0d557ef7eb949c2d298fe457816a
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-09 22:29:58 +0000

    Add missing comma

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -11,7 +11,7 @@ const Intro = (props) => {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
-        }
+        },
         body: JSON.stringify({
           firstParam: response.accessToken
         }),
