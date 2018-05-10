3a24da5969a5cc9c4d9a3bbb0f20b5cef0c5f8b9
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-10 19:07:39 +0000

    Remove unneccessary html tags; edit classNames; change intro text

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -25,13 +25,11 @@ const Intro = (props) => {
   }
 
   return (
-    <div className="intro">
-      <div className="image-text">
-        <h1>
-          <span>IF YOU CAN'T REACH YOUR PHONE,<span className='spacer'></span><br />
-          <span className='spacer'></span><span id="nudge">NUDGE</span> WILL DO IT FOR YOU.</span>
-        </h1>
-      </div>
+    <div className="Intro">
+      <h1>
+        <span>IF YOU GO DARK,<span className='spacer'></span><br />
+        <span className='spacer'></span><span id="nudge">NUDGE</span> WILL RAISE THE FLAG.</span>
+      </h1><br />
 
       <GoogleLogin
         id='google-login'
@@ -42,6 +40,7 @@ const Intro = (props) => {
       ><FontAwesome name='google' />
       <span> Login with Google</span>
       </GoogleLogin>
+
     </div>
   );
 }
