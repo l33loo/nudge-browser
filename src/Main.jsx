eb10e36ccbf49f43eb61a8a733afd69136ab6853
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-11 18:32:19 +0000

    Change to other componentDidMount, which works

--- src/Main.jsx
+++ src/Main.jsx
@@ -9,36 +9,13 @@ export default class Main extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   const userId = window.localStorage.getItem('nudge_token');
-  //   // console.log(`USERRRR ID: ${userId} and STRING ${userId} and TYPE ${typeof userId}`);
-  //   // if (userId) {
-  //     fetch(`https://nudge-server.herokuapp.com/contacts/2`)
-  //     .then((response) => {
-  //       console.log(`RESPONSE CONTACTS!!!!: ${response} and USERS ${response.users}`);
-  //       var temp = response.json();
-  //       // var blah = JSON.parse(response);
-  //       console.log(`TEMP: ${temp}`);
-  //       return temp;
-  //     })
-  //     .then((resp) => {
-  //       // console.log(`CONTACTS JSON!!!! ${resp}, USERSSS ${resp.users}`);
-  //       const newState = {};
-  //       newState["contacts"] = resp.users;
-  //       // console.log(`NEW STATE ${newState}`);
-  //       this.setState({ newState });
-  //       console.log(`NEW THIS.STATE.CONTACTS: ${this.state.contacts}`);
-  //     });
-  //   // }
-  // }
-
-  componentDidMount(){
-    return fetch(`https://nudge-server.herokuapp.com/contacts/2`)
+  componentDidMount() {
+    const userId = window.localStorage.getItem('nudge_token');
+    return fetch(`https://nudge-server.herokuapp.com/contacts/2`) //change to ${userId}
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
           contacts: responseJson.users
-        }, function(){
         });
       })
       .catch((error) =>{
