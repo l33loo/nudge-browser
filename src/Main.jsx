24469bd72c811dfe647cef7b650c0a970729ce9b
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-11 18:17:17 +0000

    Testing of server response; add as comment alernative componentDidMount to be tested later

--- src/Main.jsx
+++ src/Main.jsx
@@ -15,11 +15,11 @@ export default class Main extends Component {
     // if (userId) {
       fetch(`https://nudge-server.herokuapp.com/contacts/2`)
       .then((response) => {
-        console.log(`RESPONSE CONTACTS!!!!: ${response}`);
+        console.log(`RESPONSE CONTACTS!!!!: ${response} and USERS ${response.users}`);
         var temp = response.json();
         // var blah = JSON.parse(response);
         console.log(`TEMP: ${temp}`);
-        return response.json();
+        return temp;
       })
       .then((resp) => {
         // console.log(`CONTACTS JSON!!!! ${resp}, USERSSS ${resp.users}`);
@@ -31,6 +31,20 @@ export default class Main extends Component {
       });
     // }
   }
+
+  // componentDidMount(){
+  //   return fetch(`https://nudge-server.herokuapp.com/contacts/${this.props.screenProps.id}`)
+  //     .then((response) => response.json())
+  //     .then((responseJson) => {
+  //       this.setState({
+  //         dataSource: responseJson.users,
+  //       }, function(){
+  //       });
+  //     })
+  //     .catch((error) =>{
+  //       console.error(error);
+  //     });
+  // }
 
   render() {
     const cont = this.state.contacts.map((contact) => {
