4f9cdc89e40ec08dd7db797852c7b980693b5a36
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-11 18:27:26 +0000

    Test different componentDidMount

--- src/Main.jsx
+++ src/Main.jsx
@@ -9,42 +9,42 @@ export default class Main extends Component {
     }
   }
 
-  componentDidMount() {
-    const userId = window.localStorage.getItem('nudge_token');
-    // console.log(`USERRRR ID: ${userId} and STRING ${userId} and TYPE ${typeof userId}`);
-    // if (userId) {
-      fetch(`https://nudge-server.herokuapp.com/contacts/2`)
-      .then((response) => {
-        console.log(`RESPONSE CONTACTS!!!!: ${response} and USERS ${response.users}`);
-        var temp = response.json();
-        // var blah = JSON.parse(response);
-        console.log(`TEMP: ${temp}`);
-        return temp;
-      })
-      .then((resp) => {
-        // console.log(`CONTACTS JSON!!!! ${resp}, USERSSS ${resp.users}`);
-        const newState = {};
-        newState["contacts"] = resp.users;
-        // console.log(`NEW STATE ${newState}`);
-        this.setState({ newState });
-        console.log(`NEW THIS.STATE.CONTACTS: ${this.state.contacts}`);
-      });
-    // }
-  }
-
-  // componentDidMount(){
-  //   return fetch(`https://nudge-server.herokuapp.com/contacts/${this.props.screenProps.id}`)
-  //     .then((response) => response.json())
-  //     .then((responseJson) => {
-  //       this.setState({
-  //         dataSource: responseJson.users,
-  //       }, function(){
-  //       });
+  // componentDidMount() {
+  //   const userId = window.localStorage.getItem('nudge_token');
+  //   // console.log(`USERRRR ID: ${userId} and STRING ${userId} and TYPE ${typeof userId}`);
+  //   // if (userId) {
+  //     fetch(`https://nudge-server.herokuapp.com/contacts/2`)
+  //     .then((response) => {
+  //       console.log(`RESPONSE CONTACTS!!!!: ${response} and USERS ${response.users}`);
+  //       var temp = response.json();
+  //       // var blah = JSON.parse(response);
+  //       console.log(`TEMP: ${temp}`);
+  //       return temp;
   //     })
-  //     .catch((error) =>{
-  //       console.error(error);
+  //     .then((resp) => {
+  //       // console.log(`CONTACTS JSON!!!! ${resp}, USERSSS ${resp.users}`);
+  //       const newState = {};
+  //       newState["contacts"] = resp.users;
+  //       // console.log(`NEW STATE ${newState}`);
+  //       this.setState({ newState });
+  //       console.log(`NEW THIS.STATE.CONTACTS: ${this.state.contacts}`);
   //     });
+  //   // }
   // }
+
+  componentDidMount(){
+    return fetch(`https://nudge-server.herokuapp.com/contacts/2`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          contacts: responseJson.users
+        }, function(){
+        });
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
 
   render() {
     const cont = this.state.contacts.map((contact) => {
