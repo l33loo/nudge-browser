63657b0eb19f6e6538fa3152f6c54b96b4bc9d17
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-12 17:58:14 +0000

    Add br after contact name element

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = ({ contact }) => {
   return (
     <li className='contact'>
-      <span className='contact-name'>{ contact.nickname }</span>
+      <span className='contact-name'>{ contact.nickname }</span><br />
       <span className='contact-email'>{ contact.email }</span>
     </li>
   );
