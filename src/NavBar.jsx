b7010117f4f86a761f06c60ccf9ae752ab047971
Lila Karpowicz <karpowicz.lila@gmail.com>
2018-05-13 22:22:07 -0700

    Remove user's first name and email fro local storage upon logout

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -19,6 +19,8 @@ const NavBar = (props) => {
   const logout = () => {
     console.log("LOgoUT");
     window.localStorage.removeItem('nudge_token');
+    window.localStorage.removeItem('nudge_name');
+    window.localStorage.removeItem('nudge_email');
     props.clearState();
   };
 
